d775f3acb26258d0803252908865ca6a91cade7c
novakovic.luka <lukanovakovic2@gmail.com>
2016-03-15 12:48:48 +0100

    Napravljena klasa Biblioteka koja implementira interfejs BibliotekaInterfejs

---
+++ Dodatna/src/biblioteka/Biblioteka.java
@@ -0,0 +1,37 @@
+package biblioteka;
+
+import java.util.LinkedList;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs {
+	LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	
+	
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+	}
+
+	
+	public void obrisiKnjigu(Knjiga knjiga) {
+	knjige.remove(knjiga);
+
+	}
+
+	
+	public LinkedList<Knjiga> vratiSveKnjige() {
+		
+		return knjige;
+	}
+
+	@Override
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
+		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		for (int i = 0; i < knjige.size(); i++) {
+			if(knjige.get(i).getNaslov().contains(naslov))
+				rezultat.add(knjige.get(i));
+		}
+		return null;
+	}
+
+}
