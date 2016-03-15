cfb5cb586e971fe3f4b0886d87e5c30a41f1d91c
novakovic.luka <lukanovakovic2@gmail.com>
2016-03-15 12:58:15 +0100

    dODATA LOGICKA KONTROLA

--- Dodatna/src/biblioteka/Biblioteka.java
+++ Dodatna/src/biblioteka/Biblioteka.java
@@ -9,11 +9,16 @@ public class Biblioteka implements BibliotekaInterfejs {
 	
 	
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga == null || knjige.contains(knjiga))
+			throw new RuntimeException("Greska pri unosu knjige");
 		knjige.add(knjiga);
 	}
 
 	
 	public void obrisiKnjigu(Knjiga knjiga) {
+		if(knjiga == null || !knjige.contains(knjiga))
+			throw new RuntimeException("Greska pri brisanju knjige");
+		knjige.add(knjiga);
 	knjige.remove(knjiga);
 
 	}
@@ -21,11 +26,16 @@ public class Biblioteka implements BibliotekaInterfejs {
 	
 	public LinkedList<Knjiga> vratiSveKnjige() {
 		
+		
 		return knjige;
 	}
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
+		if(naslov == null) throw new RuntimeException("Naslov ne sme biti null");
+		
+		
+		
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
 		for (int i = 0; i < knjige.size(); i++) {
 			if(knjige.get(i).getNaslov().contains(naslov))
