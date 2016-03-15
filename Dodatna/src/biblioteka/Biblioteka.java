a600dc9ec208991d042b0703d7db1bb2093f6740
novakovic.luka <lukanovakovic2@gmail.com>
2016-03-15 13:01:14 +0100

    Uvedena logicka kontrola u metodu Vrati knjige sa naslovom

--- Dodatna/src/biblioteka/Biblioteka.java
+++ Dodatna/src/biblioteka/Biblioteka.java
@@ -26,6 +26,8 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
+		if(naslov == null) 
+			return knjige;
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
 		for (int i = 0; i < knjige.size(); i++) {
 			if(knjige.get(i).getNaslov().contains(naslov))
