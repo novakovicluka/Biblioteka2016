c4f3200a3dae41989189852c2ce66500e4c86d33
novakovic.luka <lukanovakovic2@gmail.com>
2016-03-15 13:10:15 +0100

    dodat komentar

--- Dodatna/src/biblioteka/Biblioteka.java
+++ Dodatna/src/biblioteka/Biblioteka.java
@@ -32,9 +32,8 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
-
+       // naslov knjiga ne sme biti null - ne sme bitiiiiiiiiiiiiiiiii
 		if(naslov == null) throw new RuntimeException("Naslov ne sme biti null");
-
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
 		for (int i = 0; i < knjige.size(); i++) {
 			if(knjige.get(i).getNaslov().contains(naslov))
